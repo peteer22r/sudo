7be53203a37140f0e05da9e56cba4f5f5bc7d104
peteer22r <peteer22r@o2.pl>
2019-06-13 22:42:22 +0200

    elevation of cmd.exe commands, exit after elevation of GUI process, preparation for tests

--- sudo.cpp
+++ sudo.cpp
@@ -3,23 +3,22 @@
 #include <sstream>
 #include <Shlobj.h>
 
-std::string GetLastErrorAsString()
-{
-	//Get the error message, if any.
-	DWORD errorMessageID = ::GetLastError();
-	if (errorMessageID == 0)
-		return std::string(); //No error message has been recorded
+struct LocalFreeDeleter {
+	void operator() (LPSTR ptr)
+	{
+		LocalFree(ptr);
+	}
+};
 
-	LPSTR messageBuffer = nullptr;
+std::unique_ptr<CHAR, LocalFreeDeleter> GetLastErrorAsString()
+{
+	DWORD errorMessageID = ::GetLastError();
+
+	std::unique_ptr<CHAR, LocalFreeDeleter> messageBuffer = nullptr;
 	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 		NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
 
-	std::string message(messageBuffer, size);
-
-	//Free the buffer.
-	LocalFree(messageBuffer);
-
-	return message;
+	return messageBuffer;
 }
 
 std::string join_params(int argc, const char* argv[])
@@ -31,9 +30,21 @@ std::string join_params(int argc, const char* argv[])
 	return params.str();
 }
 
+int WaitForReturnCode(HANDLE hProcess)
+{
+	DWORD rc = ERROR_SUCCESS;
+	::WaitForSingleObject(hProcess, INFINITE);
+	::GetExitCodeProcess(hProcess, &rc);
+	::CloseHandle(hProcess);
+	return rc;
+}
+
 int main(int argc, const char* argv[])
 {
-	if (IsUserAnAdmin() == FALSE)
+	size_t env_size = 0;
+	char sudo_var[5] = {};
+	getenv_s(&env_size, sudo_var, "sudo");
+	if (IsUserAnAdmin() == FALSE && strcmp(sudo_var, "sudo") != 0)
 	{
 		auto params = join_params(argc, argv);
 
@@ -45,28 +56,24 @@ int main(int argc, const char* argv[])
 		ss << GetCurrentProcessId() << ' ' << hIn << ' ' << hOut << ' ' << hErr << ' ' << params;
 		auto par = ss.str();
 
-		SHELLEXECUTEINFOA sei = {};
-		sei.fMask = SEE_MASK_NOCLOSEPROCESS | SEE_MASK_NO_CONSOLE;
-		sei.cbSize = sizeof(sei);
-		sei.lpVerb = "runas";
+		SHELLEXECUTEINFOA sei = { sizeof(sei), SEE_MASK_NOCLOSEPROCESS | SEE_MASK_NO_CONSOLE };
+		sei.lpVerb = strcmp(sudo_var, "test") != 0 ? "runas" : NULL;
 		sei.lpFile = argv[0];
 		sei.nShow = SW_HIDE;
 		sei.lpParameters = par.c_str();
+		_putenv("sudo=sudo");
+
 		if (ShellExecuteExA(&sei) == FALSE)
 		{
-			std::cerr << GetLastErrorAsString();
+			std::cerr << GetLastErrorAsString().get();
 			return EXIT_FAILURE;
 		}
 		SetConsoleCtrlHandler(NULL, TRUE);
 		if (WAIT_TIMEOUT == WaitForSingleObject(sei.hProcess, 250))
 		{
 			FreeConsole();
-			WaitForSingleObject(sei.hProcess, INFINITE);
 		}
-		DWORD dwRc = 0;
-		GetExitCodeProcess(sei.hProcess, &dwRc);
-		CloseHandle(sei.hProcess);
-		return dwRc;
+		return WaitForReturnCode(sei.hProcess);
 	}
 	else
 	{
@@ -84,15 +91,20 @@ int main(int argc, const char* argv[])
 		DuplicateHandle(hParent, (HANDLE)strtoull(argv[4], nullptr, 16), GetCurrentProcess(), &si.hStdError, 0, TRUE, DUPLICATE_SAME_ACCESS);
 		if (CreateProcessA(NULL, &params[0], NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi) == FALSE)
 		{
-			auto errorText = GetLastErrorAsString();
-			BOOL rc = WriteFile(si.hStdError, errorText.c_str(), errorText.length(), NULL, NULL);
-			return EXIT_FAILURE;
+			params.insert(0, "cmd.exe /C ");
+			if (GetLastError() != ERROR_FILE_NOT_FOUND || CreateProcessA(NULL, &params[0], NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi) == FALSE)
+			{
+				auto errorText = GetLastErrorAsString();
+				BOOL rc = WriteFile(si.hStdError, errorText.get(), strlen(errorText.get()), NULL, NULL);
+				return EXIT_FAILURE;
+			}
 		}
 		FreeConsole();
-		WaitForSingleObject(pi.hProcess, INFINITE);
-		DWORD dwRc = 0;
-		GetExitCodeProcess(pi.hProcess, &dwRc);
-		CloseHandle(pi.hProcess);
-		return dwRc;
+		if (AttachConsole(pi.dwProcessId) != FALSE || GetLastError() != ERROR_INVALID_HANDLE)
+		{
+			FreeConsole();
+			return WaitForReturnCode(pi.hProcess);
+		}
+		return EXIT_SUCCESS;
 	}
 }
